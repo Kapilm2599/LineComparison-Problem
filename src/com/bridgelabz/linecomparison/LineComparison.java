37ad0811506f4089e55e41d46ff572a01c0a2d36
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-14 15:11:09 +0530

    Added: Comparing line using Oops concept

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -4,40 +4,42 @@ import java.util.Scanner;
 
 public class LineComparison {
 	
-	public static void main(String[] args) {
-		System.out.println("Welcome to the Line Compression Program");
+	static double length1, length2, length;
+	static int x1, x2, y1, y2;
+
+	public void coordinates() {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Value X1 X2 Y1 Y2");
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the value of X1 X2 X3 X4");
+		int x1 = scanner.nextInt();    
+		int x2 = scanner.nextInt();
+		int y1 = scanner.nextInt();
+		int y2 = scanner.nextInt();
 
-		int x1 = sc.nextInt();
-		int x2 = sc.nextInt();
-		int x3 = sc.nextInt();
-		int x4 = sc.nextInt();
+		length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+	}
+
+	public void getLenght() {
+		coordinates();
+		length1 = length;
+		System.out.println("Enter Value For second Line: ");
+		coordinates();
+		length2 = length;
+		System.out.println("Length of Line1: " + length1);
+		System.out.println("Length of Line2: " + length2);
+	}
+
+	public void compareLines() {
+		// value = lenOfLineFirst.compareTo(lenOfLineSecond);
+		if (length1 == length2)    
+			System.out.println("Both lines are Equal.");
+		else if (length1 > length2)
+			System.out.println("Line1 is greater than Line2.");
+		else
+			System.out.println("Line1 is smaller than Line2.");
 		
-		System.out.println("Enter the value of Y1 Y2 Y3 Y4");
-		int y1 = sc.nextInt();
-		int y2 = sc.nextInt();
-		int y3 = sc.nextInt();
-		int y4 = sc.nextInt();
 
-		double length1;
-		double length2;
-
-		length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-		System.out.println("Length of line1: " + length1);
-		length2 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
-		System.out.println("Length of Line 2: " + length2);
-
-		if (length1 == length2) {
-			System.out.println("Both Length are Equal");
-		} else if (length1 > length2) {
-			System.out.println("Length of first line is Greater");
-		} else {
-			System.out.println("Length of second line is Greater");
-		}
-
-	
 	}
 
 }
---
+++ src/com/bridgelabz/linecomparison/LineComparisonMain.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.linecomparison;
+
+public class LineComparisonMain {
+	
+	public static void main(String[] args) {
+		
+		LineComparison linecomparison = new LineComparison();  
+		
+		linecomparison.getLenght();
+		linecomparison.compareLines();
+		
+	}
+
+}
